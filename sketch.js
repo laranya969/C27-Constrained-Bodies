f23832d56d573fe78fb1f8909106055a7300d8e7
Laranya <laranyakhairnar@gmail.com>
2021-01-05 17:52:15 +0530

    change

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain, log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,7 +36,13 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    log6 = new Log(200, 300, 30, PI/2);
+
     bird = new Bird(100,100);
+
+    chain = new Chain(bird.body, log6.body);
+
+
 
 }
 
@@ -61,4 +69,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    chain.display();
 }
